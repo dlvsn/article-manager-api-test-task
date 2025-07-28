2f9344c23e823fa3e815c2e63f8c857d53929a08
Denys Mazurenko <0gur4ik3992@gmail.com>
2025-07-28 09:15:25 +0300

    add project infastructure

---
+++ src/main/java/denys/mazurenko/ArticleManagerApiTestTaskApplication.java
@@ -0,0 +1,13 @@
+package denys.mazurenko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticleManagerApiTestTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArticleManagerApiTestTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/denys/mazurenko/ArticleManagerApiTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package denys.mazurenko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticleManagerApiTestTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
